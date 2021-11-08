a3cbad9e8fb3363d901490bf49f347f6f892886e
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-11-08 09:15:41 +0900

    Exhaustive search: exhaustive search of array 8: simple solution

---
+++ exhaustiveSearch/array/8/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	t.Parallel()
+	type in struct {
+		n int
+		a []int
+	}
+	tests := []struct {
+		in   in
+		want int
+	}{
+		{in: in{n: 5, a: []int{-1, 3, 5, -7, -9}}, want: -9},
+	}
+	for i, tt := range tests {
+		i, tt := i, tt
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			got := solution(tt.in.n, tt.in.a)
+			if got != tt.want {
+				t.Fatalf("in: %v got: %v want: %v", tt.in, got, tt.want)
+			}
+		})
+	}
+}
+
+func Example_main() {
+	fd, _ := os.Open(filepath.Join("testdata", "input.txt"))
+	orgStdin := os.Stdin
+	os.Stdin = fd
+	defer func() {
+		os.Stdin = orgStdin
+		fd.Close()
+	}()
+
+	main()
+
+	// Output:
+	// -9
+}
