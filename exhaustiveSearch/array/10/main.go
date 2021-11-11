38e4bcb93eb17c501f117da50491012d8a049696
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-11-11 10:42:41 +0900

    Exhaustive search: exhaustive search of array 10: simple solution attempts: 2

---
+++ exhaustiveSearch/array/10/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solution(n int, a []int) int {
+	memo := make([]int, 10)
+	for i := range a {
+		memo[a[i]]++
+	}
+
+	max := -1<<31 - 1
+	out := 0
+	for i := range memo {
+		if max < memo[i] {
+			max = memo[i]
+			out = i
+		}
+	}
+	return out
+}
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(r, &n)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Fscan(r, &a[i])
+	}
+	fmt.Println(solution(n, a))
+}
