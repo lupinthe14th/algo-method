082f0fc234f0841a49e6a4054e54de26855e8dbf
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2021-11-22 10:25:15 +0900

    Exhaustive search: exhaustive search of number 1: refacter code

--- exhaustiveSearch/number/1/main.go
+++ exhaustiveSearch/number/1/main.go
@@ -9,16 +9,9 @@ import (
 func solution(n int) int {
 	out := 0
 	for i := 1; i <= n; i++ {
-		if i%2 == 0 {
-			continue
+		if i%2 != 0 && i%3 != 0 && i%5 != 0 {
+			out++
 		}
-		if i%3 == 0 {
-			continue
-		}
-		if i%5 == 0 {
-			continue
-		}
-		out++
 	}
 	return out
 }
